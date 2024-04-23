0078d3db7aeb2463a30b147a15a039cfd37338d3
Nicolas Parroquiano <nicolasparroquiano@gmail.com>
2024-04-23 10:51:25 -0500

    BoxCounter NetworkBehaviour Test 2

--- Assets/Scripts/Props/BoxCounter.cs
+++ Assets/Scripts/Props/BoxCounter.cs
@@ -11,44 +11,50 @@ public class BoxCounter : NetworkBehaviour
 {
     public TextMeshProUGUI counterText;
     public Image counterIcon; // Asume que tienes un Image UI para el ícono
-
     private string[] layers = {"Phone", "Picture", "Cup"};
-    private string selectedLayer;
-    [SerializeField]private int count = 0;
-    [SerializeField]private int totalCount = 1;
+    //private string selectedLayer;
+    //[SerializeField]private int count = 0;
+    private NetworkVariable<string> selectedLayer = new NetworkVariable<string>();
+    private NetworkVariable<int> count = new NetworkVariable<int>();
+    private NetworkVariable<int> totalCount = new NetworkVariable<int>();
+    //[SerializeField]private int totalCount = 1;
     public Door door;
-
     public Sprite spritePhone, spritePicture, spriteCup;
 
     public override void OnNetworkSpawn()
     {
-        if(!IsServer)
+        if (!IsOwner)
         {
-            enabled=false;
+            enabled = false; 
             return;
         }
-        selectedLayer = layers[UnityEngine.Random.Range(0, layers.Length)];
-        int layerIndex = LayerMask.NameToLayer(selectedLayer);
-        totalCount = FindObjectsOfType<GameObject>().Count(g => g.layer == layerIndex);
-        
-        UpdateUI(0);
-        SetIconForSelectedLayer(); 
+
+        SendStartValuesRpc();
     }
     void Start()
     {
         
     }
 
-
+    [Rpc(SendTo.Server)]
+    void SendStartValuesRpc()
+    {
+        selectedLayer.Value = layers[UnityEngine.Random.Range(0, layers.Length)];
+        int layerIndex = LayerMask.NameToLayer(selectedLayer.Value);
+        totalCount.Value = FindObjectsOfType<GameObject>().Count(g => g.layer == layerIndex);
+        
+        UpdateUI(0);
+        SetIconForSelectedLayer();
+    }
 
     void UpdateUI(int currentCount)
     {
-        counterText.text = currentCount + "/" + totalCount;
+        counterText.text = currentCount + "/" + totalCount.Value;
     }
 
     void SetIconForSelectedLayer()
     {
-         switch (selectedLayer)
+         switch (selectedLayer.Value)
         {
             case "Phone":
                 counterIcon.sprite = spritePhone;
@@ -62,25 +68,36 @@ public class BoxCounter : NetworkBehaviour
         }
     }
 
+    [Rpc(SendTo.Server)]
+    void OnTriggerEnterRpc()
+    {
+        count.Value += 1;
+        UpdateUI(count.Value);
+        if(count == totalCount)
+        {
+        door.OpenDoorRpc();
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    void OnTriggerExitRpc()
+    {
+        count.Value -= 1;
+        UpdateUI(count.Value);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer(selectedLayer))
+        if (other.gameObject.layer == LayerMask.NameToLayer(selectedLayer.Value))
         {
-            count += 1;
-            UpdateUI(count);
-            if(count == totalCount)
-            {
-            door.OpenDoorRpc();
-            }
+            OnTriggerEnterRpc();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer(selectedLayer))
+        if (other.gameObject.layer == LayerMask.NameToLayer(selectedLayer.Value))
         {
-            count -= 1;
-            UpdateUI(count);
+            OnTriggerExitRpc();
         }
     }
 
