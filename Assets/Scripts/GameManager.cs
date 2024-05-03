302f53f160ced9270b67ac56354f23b37192cfea
Juan Hernández <juanhernandeznz@gmail.com>
2024-05-03 14:50:32 -0500

    fix stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : NetworkBehaviour
     private NetworkVariable<int> keys = new NetworkVariable<int>();
     public NetworkVariable<bool> shadowPuzle = new NetworkVariable<bool>();
     private NetworkVariable<float> timeLeft = new NetworkVariable<float>();
-    private float initialTime = 1 * 60;
+    private float initialTime = 2 * 60;
     public float localTime;
     private const int initialKeys = 0;
     public TextMeshProUGUI timeUIText;
@@ -57,7 +57,11 @@ public class GameManager : NetworkBehaviour
         if (gameStarted && Input.GetKeyDown(KeyCode.P) && pausePanel != null)
         {
             ChangePauseState();
-            mainDoor.OpenDoorRpc();
+        }
+        if (localTime  < 0)
+        {
+            losePanel.SetActive(true );
+            EnableCursor();
         }
     }
 
