73a3e9cc5c8da38888e9e97266b8406a081b5899
Juan Hernández <juanhernandeznz@gmail.com>
2024-05-03 13:53:22 -0500

    fix portal

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : NetworkBehaviour
     private NetworkVariable<int> keys = new NetworkVariable<int>();
     public NetworkVariable<bool> shadowPuzle = new NetworkVariable<bool>();
     private NetworkVariable<float> timeLeft = new NetworkVariable<float>();
-    public float initialTime = 60;
+    public float initialTime = 15 * 60;
     public float localTime;
     private const int initialKeys = 0;
     public TextMeshProUGUI timeUIText;
